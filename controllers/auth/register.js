98c127d63a969d7a4c2fc1a516bcc6b74f2ddd81
El-dorado01 <102257373+El-dorado01@users.noreply.github.com>
2023-07-20 11:08:37 +0100

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -175,6 +175,7 @@ const registerUser = asyncWrapper(async (req, res) => {
         };
 
         const result = await transporter.sendMail(message);
+      console.log(accessToken);
         return result;
     } catch (err) {
         return err;
@@ -194,7 +195,7 @@ const registerUser = asyncWrapper(async (req, res) => {
 
 sendMail().then(result => {
     // =====================Email Verification Sent==================== //
-    console.log("Registration successfully");
+    console.log("Registration successfully", result);
 }).catch(err => {
     console.error("An error occurred: ", err)
 })
